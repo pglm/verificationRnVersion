3c31856e20291a9de7770a9bcd92baf13210d7b2
wgpeng <wgpeng@vova.com.hk>
2020-10-07 15:36:01 +0800

    插件1.0

---
+++ VerificationRnVersion/src/main/groovy/com/mc_android/verificationRnVersion/VerificationRnVersion.groovy
@@ -0,0 +1,47 @@
+package com.mc_android.verificationRnVersion
+
+import groovy.json.JsonSlurper
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+
+class VerificationRnVersion implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+        project.extensions.create("rnPackageJson", PackageJson)
+        project.task('verificationRnVersion'){
+            println "开始验证rn版本和module里面的版本是否一致"
+            def packageJsonFile = file(project.rnPackageJson.localRnPath)
+            def packageJson = new JsonSlurper().parseText(packageJsonFile.text)
+            def packageVvRn=packageJson.dependencies['vv-rn']
+            println packageVvRn
+
+            def vvRnPackageJsonFile = file(project.rnPackageJson.remoteRnPath)
+            def vvRnPackageJson  = new JsonSlurper().parseText(vvRnPackageJsonFile.text)
+            def vvRnPackageVvRn=vvRnPackageJson['version']
+            println vvRnPackageVvRn
+
+            if(packageVvRn == vvRnPackageVvRn){
+                println "rn版本和module里面的版本是一致的："+packageVvRn
+            }else{
+                throw RuntimeException("请去更新rn的node_modules包,然后再打包")
+            }
+        }
+
+
+        project.tasks.whenTaskAdded { Task theTask ->
+            if (theTask.name.contains('assemble')) {
+                theTask.dependsOn(verificationRnVersion)
+                theTask.mustRunAfter(verificationRnVersion)
+            }
+        }
+    }
+}
+
+
+class PackageJson {
+    String localRnPath = ''
+    String remoteRnPath = ''
+}
